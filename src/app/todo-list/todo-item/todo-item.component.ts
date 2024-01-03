718c675cf5e9b4cd346ca6f7f9cbd089148cf3c2
Kohei Akiyama <kohei.akiyama@outlook.jp>
2024-01-03 12:42:16 +0900

    デザイン変更

--- src/app/todo-list/todo-item/todo-item.component.ts
+++ src/app/todo-list/todo-item/todo-item.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss',
 })
 export class TodoItemComponent {
   done = false;
+  title = '牛乳を買う';
 
   onToggle(): void {
     this.done = !this.done;
