42ba138cbbc514b6744a1c371c0961dbe5513709
Kohei Akiyama <kohei.akiyama@outlook.jp>
2024-01-28 20:14:07 +0900

    修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CommonModule, RouterOutlet, MainLayoutComponent, TodoListComponent],
 })
 export class AppComponent {
   title = 'GTD ToDo';
