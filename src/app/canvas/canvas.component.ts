c8f499589b3dcada7b7852621344bb220dd3800b
Kohei Akiyama <kohei.akiyama@outlook.jp>
2024-03-10 17:23:38 +0900

    change

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   standalone: true,
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
   templateUrl: './canvas.component.html',
   styleUrl: './canvas.component.scss',
 })
-export class CanvasComponent {}
+export class CanvasComponent {
+  @ViewChild('canvas') canvas!: ElementRef<HTMLCanvasElement>;
+
+  onCLick() {
+    console.log(this.canvas);
+    const context = this.canvas.nativeElement.getContext('2d');
+    if (context) {
+      context.fillStyle = 'red';
+      context.fillRect(10, 10, 50, 50);
+
+      context.fillStyle = 'rgba(0, 0, 200, 0.5)';
+      context.fillRect(30, 30, 50, 50);
+      context.font = '"80px serif';
+      context.fillText('Hello World', 400, 400);
+    }
+  }
+}
